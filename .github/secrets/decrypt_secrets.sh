83dbb42f95c6f3ebdb06e476b2203386de6c5b72
brampurnot <bpurnot@icloud.com>
2023-05-18 10:54:43 +0200

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -14,7 +14,7 @@ security default-keychain -s build.keychain
 security import ./.github/secrets/LH-SelfSignedCertificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
-#security default-keychain -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
 #security unlock-keychain -p "lh-1234" ~/Library/Keychains/build.keychain
 
 #security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
