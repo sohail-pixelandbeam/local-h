61cb2c5348d44da2d8928fc787d0293b2aed6762
brampurnot <bpurnot@icloud.com>
2023-05-18 10:35:37 +0200

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -9,8 +9,8 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/lhprovisioningprofile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/lhprovisioningprofile.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/LH-SelfSignedCertificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$KEYCHAIN_PWD" -A
+security create-keychain -p "lh-1234" build.keychain
+security import ./.github/secrets/LH-SelfSignedCertificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "lh-1234" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
