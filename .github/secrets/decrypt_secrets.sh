a1b704679ee5d5b9a89726e36beeabca8714a633
brampurnot <bpurnot@icloud.com>
2023-05-18 11:32:34 +0200

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -eo pipefail
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/lhprovisioningprofile.mobileprovision ./.github/secrets/lhprovisioningprofile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/LH-SelfSignedCertificate.p12 ./.github/secrets/LH-SelfSignedCertificate.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/LocalHappinezProv.mobileprovision ./.github/secrets/LocalHappinezProv.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/lhprovisioningprofile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/lhprovisioningprofile.mobileprovision
+cp ./.github/secrets/LocalHappinezProv.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/LocalHappinezProv.mobileprovision
 
 
 security create-keychain -p "lh-1234" build.keychain
 security default-keychain -s build.keychain
-security import ./.github/secrets/LH-SelfSignedCertificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
