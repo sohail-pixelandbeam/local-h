2f52cbf001dd3aae92318cb8a2655419967f6c18
brampurnot <bpurnot@icloud.com>
2023-05-24 08:48:39 +0200

    Update export_ipa.sh

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -5,4 +5,5 @@ set -eo pipefail
 xcodebuild -archivePath $PWD/build/eventapp.xcarchive \
             -exportOptionsPlist ./ios/exportOptions.plist \
             -exportPath $PWD/build \
+            -allowProvisioningUpdates \
             -exportArchive | xcpretty
